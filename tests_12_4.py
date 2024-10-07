9ec6b3938fc544c7e14d4daf2f944884211c2523
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-10-07 21:08:07 +0700

    Update tests_12_4.py

--- tests_12_4.py
+++ tests_12_4.py
@@ -15,16 +15,15 @@ class RunnerTest(unittest.TestCase):
         self.obj_walk = runner.Runner('Ann', 5)
         self.obj_run = runner.Runner('Maks', 5)
 
-    def test_walk(self):
+        def test_walk(self):
         """Test for walk"""
         try:
             for _ in range(10):
                 self.obj_walk.walk()
             self.assertEqual(self.obj_walk.distance, 50)
             logging.warning('"test_walk" выполнен успешно.')
-        except TypeError and ValueError:
-            logging.error('Неверная скорость для Runner', exc_info=True)  # логируйте его на уровне WARNING
-            return None
+        except TypeError and ValueError as err:
+            logging.error(err,'Неверная скорость для Runner', exc_info=True)  # логируйте его на уровне WARNING
 
     def test_run(self):
         """Test for run"""
@@ -33,9 +32,8 @@ class RunnerTest(unittest.TestCase):
                 self.obj_run.run()
             self.assertEqual(self.obj_run.distance, 100)
             logging.warning('"test_run" выполнен успешно.')
-        except TypeError and ValueError:
-            logging.error('Неверный тип данных для объекта Runner', exc_info=True)  # логируйте его на уровне WARNING
-            return None
+        except TypeError and ValueError as err:
+            logging.error(err, 'Неверный тип данных для объекта Runner', exc_info=True)  # логируйте его на уровне WARNING
 
     def test_challenge(self):
         """Test walk == run"""
